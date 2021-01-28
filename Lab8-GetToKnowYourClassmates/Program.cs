b7db27f6a81deaddbe603a7b52d826a18ba32c47
Jeffrey <JWohlfield@hotmail.com>
2021-01-27 20:26:18 -0500

    Initial commit

---
+++ Lab8-GetToKnowYourClassmates/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab8_GetToKnowYourClassmates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>()
+            {
+                "Ramon", "Antonio", "Joshua", "Nick", "Jeremiah", "Wendi", "Juliana",
+                "Nathaniel", "Tommy", "Grace", "Jeffrey", "Josh", "Stephen"
+            };
+            
+            List<string> towns = new List<string>()
+            {
+                "Tigard, OR", "Beverly Hills, MI", "Novi, MI", "Canton, MI", "Crystal, MI", "Detroit, MI",
+                "Denver, CO", "Berkley, MI", "Raleigh, NC", "Mesa, AZ", "Detroit, MI", "Baldwin, MI", "West Philadelphia, PA"
+            };
+
+            List<string> food = new List<string>()
+            {
+                "Burgers", "Focaccia Barese", "Naleski", "Tacos", "Burgers", "Salami", "Tacos", "Steak", "Chicken Curry",
+                "Sweet Potato Fries", "Steak", "Falafel", "Knuckle Sandwiches"
+            };
+
+            //Console.WriteLine($"Names:\n{String.Join("\n", names)}");
+            Console.WriteLine("Welcome to the Grand Circus C# class!");
+
+            string studentName; int studentID;
+
+            while (true)
+            {
+                Console.Write($"Which student would you like to lean more about? (enter a number 1-{names.Count}) ");
+                string input = Console.ReadLine();
+
+                studentID = GetStudentID(names, input);
+
+                if(studentID == -1)
+                {
+                    continue;
+                }
+
+                studentName = names[studentID];
+                Console.WriteLine($"Student {studentID} is {studentName}. What would you like to know about {studentName}?");
+                    do
+                    {
+                        Console.Write($"(enter \"hometown\" or \"favorite food\") ");
+                        string response = Console.ReadLine().ToLower();
+
+                        if(response == "hometown")
+                         {
+                            Console.WriteLine($"{studentName} is from {towns[studentID]}.\n");
+                         }
+                        else if(response == "favorite food")
+                         {
+                            Console.WriteLine($"{studentName} loves {food[studentID]}.\n");
+                         }
+                        else
+                         {
+                            Console.WriteLine("That data does not exist.  Please try again.");
+                            continue;
+                         }
+
+                            Console.Write("Would you like to know more? ");
+                            if (Console.ReadLine().ToLower() != "y")
+                            {
+                            break;
+                            }
+                    } while (true);
+                
+                Console.Write("Would you like to check another student? ");
+                if(Console.ReadLine().ToLower() != "y")
+                {
+                    break;
+                }
+            }
+        }
+
+        public static int GetStudentID(List<string> names, string input)
+        {
+            int selection;
+            try
+            {
+                selection = int.Parse(input);
+                
+                return selection;
+
+            }
+            catch (FormatException e1)
+            {
+                Console.WriteLine("You didn't enter a number");
+                Console.WriteLine(e1.Message);
+                Console.WriteLine("Let's try again");
+            }
+            catch (ArgumentOutOfRangeException e2)
+            {
+                Console.WriteLine("That student does not exist");
+                Console.WriteLine(e2.Message);
+                Console.WriteLine("Let's try again");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Let's try again");
+                return -1;
+            }
+            return -1;
+        }
+
+    }
+}
